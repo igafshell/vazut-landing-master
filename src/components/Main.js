b98eed3b6eef70556cc662f53c1babea79fbd0ab
Dan <77572373+igafshell@users.noreply.github.com>
2023-06-20 12:30:45 +0300

    changed choose-us section

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,13 +2,14 @@ import site_workers_illustration from "../assets/site-workers-illustration.svg";
 import arrow_img from "../assets/arrow-img.svg";
 import wave_img from "../assets/wave-img.svg";
 import symbol_img from "../assets/symbol-img.svg";
-import stroke from "../assets/stroke.svg";
-import stone_img from "../assets/stone-img.svg";
-import flower_img from "../assets/flower-img.svg";
 import leaf1_img from "../assets/leaf1-img.svg";
 import leaf2_img from "../assets/leaf2-img.svg";
 import yellow_obj_img from "../assets/yellow-obj-img.svg";
 import circle_img from "../assets/circle-img.svg";
+import stone_img from "../assets/stone-img.svg";
+import flower_img from "../assets/flower-img.svg";
+import website_making_illustration from "../assets/making-website-illustration.svg";
+import delivery_illustration from "../assets/delivery-illustration.svg";
 
 const Main = () => {
 	return (
@@ -38,44 +39,31 @@ const Main = () => {
 			<section className="choose-us">
 				<div className="choose-us__h1">
 					<h1>Why choose us</h1>
-					<img src={stroke} alt="" className="stroke" />
 				</div>
-				<div className="reasons">
-					<div className="reasons__1">
-						<div className="num">1</div>
-						<div className="text">
-							<h3>Power</h3>
-							<p>
-								Our websites will highlight the quality and performance of your
-								company, making your offer irresistible.
-							</p>
-						</div>
+				<div className="reason">
+					<div className="reason__text1 text">
+						Our websites will highlight the quality of your company and help you
+						easily stand out in the market.
 					</div>
-					<div className="reasons__2">
-						<div className="num">2</div>
-						<div className="text">
-							<h3>Uniqueness</h3>
-							<p>
-								We create unique designes that will help you easily stand out in
-								the market.
-							</p>
-						</div>
-					</div>
-					<div className="reasons__3">
-						<div className="num">3</div>
-						<div className="text">
-							<h3>Delivery</h3>
-							<p>
-								We assure you of quick delivery of our product, with consistent
-								and frequent updates on the progress.
-							</p>
-						</div>
+					<img
+						src={website_making_illustration}
+						alt="Another illustration of devs making a website"
+						className="website_making_illustration"
+					/>
+					<img
+						src={delivery_illustration}
+						alt="Illustration of delivery"
+						className="delivery_illustration"
+					/>
+					<div className="text reason__text2 ">
+						Also, we assure you of quick delivery of our producs, with
+						consistent and frequent updated on the progress.
 					</div>
 				</div>
 				<div className="image-collection-1">
 					<img src={yellow_obj_img} alt="" className="yellow_obj_img" />
-					<img src={stone_img} alt="" className="stone_img" />
 					<img src={leaf1_img} alt="" className="leaf1_img" />
+					<img src={stone_img} alt="" className="stone_img" />
 				</div>
 				<div className="image-collection-2">
 					<img src={leaf2_img} alt="" className="leaf2_img" />
